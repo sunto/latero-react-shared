69a465fac68884eea46aac78f0865ece907a290d
Sunny To <sunny@laterolabs.com>
2016-02-25 16:25:27 -0800

    setting up babel and import redux

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Redux from 'redux'
+
+export const testConstFunction = () => {
+  return "test const function called."
+}
+//
+export default {
+  printMsg: () => {console.log("This is a default print message.")}
+}
