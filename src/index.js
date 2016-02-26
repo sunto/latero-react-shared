8612037ed162cdec9b6d8c7a154244ca56ba6e00
Sunny To <sunny@laterolabs.com>
2016-02-25 16:27:06 -0800

    clean up code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Redux from 'redux'
 export const testConstFunction = () => {
   return "test const function called."
 }
-//
+
 export default {
   printMsg: () => {console.log("This is a default print message.")}
 }
